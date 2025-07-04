d7860f40d402353c2c8777661d9de8b47dfcc01b
jhatfiel <jdhatfield@gmail.com>
2025-07-04 08:18:09 +0000

    done

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
 
-let language = "React";
-let moon = "🌙";
+function Header() {
+  return (
+    <header>
+      <h1>Eve's Kitchen</h1>
+    </header>
+  )
+}
 
 function App() {
-  return (
-    <h1>
-      Hello {language.toUpperCase()} {moon}!!
-    </h1>
+  return (<div>
+    <Header />
+    <main>
+      <h2>We serve the most delicious food around</h2>
+    </main>
+    </div>
   );
 }
 
