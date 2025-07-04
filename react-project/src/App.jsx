b8a79b8fdbb2d771735e17cc9bc5b5d9f7b84a3d
jhatfiel <jdhatfield@gmail.com>
2025-07-04 16:24:52 +0000

    done

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,21 +1,35 @@
+/* eslint-disable react/prop-types */
 import "./App.css";
 
 function Header({ name, year }) {
   return (
     <header>
-      <h1>{name}'s Kitchen</h1>
+      <h1>{name}&apos;s Kitchen</h1>
       <p>Copyright {year}</p>
     </header>
   );
+}
+
+const items = [
+  'Macaroni and Cheese',
+  'Salmon with Potatoes',
+  'Tofu with Vegetables'
+]
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish, ind) => <li key={ind} style={{listStyle: "none"}}>{dish}</li>)}
+    </ul>
+    
+  )
 }
 
 function App() {
   return (
     <div>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <main>
-        <h2>We serve the most delicious food around</h2>
-      </main>
+      <Main dishes={items}/>
     </div>
   );
 }
