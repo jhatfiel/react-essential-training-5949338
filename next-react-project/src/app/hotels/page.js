f6b6bfb545388adf4c2d91429c3b5abd20836b8f
jhatfiel <jdhatfield@gmail.com>
2025-07-07 13:34:06 +0000

    done

--- next-react-project/src/app/hotels/page.js
+++ next-react-project/src/app/hotels/page.js
@@ -1,8 +1,26 @@
-export default function Page() {
+async function getData() {
+  const res = await fetch('https://snowtooth-hotel-api.fly.dev');
+  return res.json();
+}
+
+function HotelBlock({name, capacity}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Capacity: {capacity}</p>
+    </div>
+  )
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
     <main>
       <div>
         <h1>Hotel Details</h1>
+        {data.map((hotel)=> (
+          <HotelBlock key={hotel.id} name={hotel.name} capacity={hotel.capacity}/>
+        ))}
       </div>
     </main>
   );
