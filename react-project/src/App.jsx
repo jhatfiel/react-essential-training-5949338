0805e2ca40512b69df8c955f7bd2e7686456c2fc
jhatfiel <jdhatfield@gmail.com>
2025-07-07 12:01:23 +0000

    done

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./App.css";
 import chef from "./images/chef.jpg";
@@ -22,11 +24,12 @@ const dishObjects = items.map((dish, i) => ({
   title: dish
 }));
 
-function Main({ dishes }) {
+function Main({ dishes, openStatus, onStatus }) {
   return (
     <>
       <div>
-        <h2>Welcome to this beautiful restaurant!</h2>
+        <button onClick={() => onStatus(true)}>I wato be open</button>
+        <h2>Welcome to this beautiful restaurant! {openStatus?'Open':'Closed'}</h2>
       </div>
       <main>
         <img
@@ -62,7 +65,7 @@ function App() {
         {status ? "Close" : "Open"} Restaurant
       </button>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <Main dishes={dishObjects} openStatus={status} onStatus={setStatus}/>
     </div>
   );
 }
